14e4e09e816a29a58360f4be5e13bb3b4fc5daef
oleksandr.nikitin <oleksandr.nikitin@engagepoint.com>
2016-06-02 12:50:28 +0300

    Added Google Maps Api

--- js/app.js
+++ js/app.js
@@ -95,5 +95,64 @@ $('.content-gallery').each(function() { // the containers for all your galleries
     });
 });
 
+function initialize() {
+    //получаем наш div куда будем карту добавлять
+    var mapCanvas = document.getElementById('map_canvas');
+    // задаем параметры карты
+    var mapOptions = {
+        //Это центр куда спозиционируется наша карта при загрузке
+        center: new google.maps.LatLng(50.179119, 30.314610),
+        //увеличение под которым будет карта, от 0 до 18
+        // 0 - минимальное увеличение - карта мира
+        // 18 - максимально детальный масштаб
+        zoom: 14,
+        //Тип карты - обычная дорожная карта
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    //Инициализируем карту
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    //Объявляем массив с нашими местами и маркерами
+    var markers = [],
+        myPlaces = [];
+    //Добавляем места в массив
+    myPlaces.push(new Place('Киев', 50.453242, 30.525513, 'Столица Украины'));
+    myPlaces.push(new Place('Васильків', 50.179119, 30.314610, 'Десь тут живу я )) Julietta Photographer'));
+
+    //Теперь добавим маркеры для каждого места
+    for (var i = 0, n = myPlaces.length; i < n; i++) {
+        var marker = new google.maps.Marker({
+            //расположение на карте
+            position: new google.maps.LatLng(myPlaces[i].latitude, myPlaces[i].longitude),
+            map: map,
+            //То что мы увидим при наведении мышкой на маркер
+            title: myPlaces[i].name
+        });
+        //Добавим попап, который будет появляться при клике на маркер
+        var infowindow = new google.maps.InfoWindow({
+            content: '<h1 class="map-marker">' + myPlaces[i].name + '</h1><br/>' + myPlaces[i].description
+        });
+        //привязываем попап к маркеру на карте
+        makeInfoWindowEvent(map, infowindow, marker);
+        markers.push(marker);
+    }
+    infowindow.open(map, marker);
+}
+function makeInfoWindowEvent(map, infowindow, marker) {
+    //Привязываем событие КЛИК к маркеру
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, myPlaces[2]);
+    });
+}
+//Это класс для удобного манипулирования местами
+function Place(name, latitude, longitude, description){
+    this.name = name;  // название
+    this.latitude = latitude;  // широта
+    this.longitude = longitude;  // долгота
+    this.description = description;  // описание места
+}
+//Когда документ загружен полностью - запускаем инициализацию карты.
+google.maps.event.addDomListener(window, 'load', initialize);
+
 
 });
